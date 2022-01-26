fe98330dee6d9a05ddb9553eb5a59ceacdd164d5
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-26 18:50:31 +0100

    Secondo Milestone completato

--- js/main.js
+++ js/main.js
@@ -21,3 +21,31 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
+
+let slides = '';
+let thumbs = '';
+
+for (let i = 0; i < items.length; i++) {
+
+    slides +=  `<div class="item">
+                    <img src="${items[i]}">
+                    
+                    <div class="bio">
+                        <h3>${title[i]}</h3>
+                        <span>${text[i]}</span>
+                    </div>
+                    
+                </div> `;
+
+    thumbs += `<div class="item-thumbs">
+                    <img src="${items[i]}">
+                </div> `;
+}
+
+//creo gli items con la variabile slides
+let itemsContainer = document.querySelector('.items-container');
+itemsContainer.innerHTML = slides;
+
+//creo le thumbs
+let thumbsContainer = document.querySelector('.thumbs-container');
+thumbsContainer.insertAdjacentHTML ("afterbegin", thumbs);
